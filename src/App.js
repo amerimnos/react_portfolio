53dabc869e1a653f05866b71e5aadfa9dbca80fe
yanggitak <yanggitak@naver.com>
2021-12-24 02:22:21 +0900

    리엑트 github 페이지에 배포 완료.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello!!!!!!!!!!~! world.
+        hello!!!!!!!!!!~! world. ...
       </header>
     </div>
   );
