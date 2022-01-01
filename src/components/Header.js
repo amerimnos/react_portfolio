fe4888677a4080d1f01ea4435cb34d1e73907aa7
yanggitak <yanggitak@naver.com>
2022-01-02 00:35:08 +0900

    header 짝대기 모션 스크립트 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,52 @@
+import { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
 
-    const clickStyle = { color: "red" };
+    const clickStyle = { color: "#2bae29" };
+    const line = useRef(null);
+
+    function lineMove(e) {
+
+        line.current.classList.add('on');
+
+        //우선 마우스 올릴시 요소의 left,top 좌표 구함
+        let elLeft = e.target.getBoundingClientRect().left;
+        let elTop = e.target.getBoundingClientRect().top;
+
+        //부모의 left,top 좌표 구함
+        let parentLeft = e.target.closest('.gnb').getBoundingClientRect().left;
+        let parentTop = e.target.closest('.gnb').getBoundingClientRect().top;
+
+        //부모와 해당 요소의 left, top을 뺌.
+        let moveLeft = elLeft - parentLeft;
+        let moveTop = elTop - parentTop;
+
+        // 짝대기에 위치값 대입
+        line.current.style.left = moveLeft + 'px';
+        line.current.style.top = moveTop+30 + 'px';
+
+        //짝대기 넓이를 마우스 올릴때 해당 요소의 크기만큼 변경하자.
+        //마우스 올린 해당 요소의 넓이 구하기.
+        let elwidth = e.target.getBoundingClientRect().width;
+
+        //짝대기 넓이에 대입하기.
+        line.current.style.width = elwidth+'px';
+
+    }
+
+
     return (
         <header>
             <div className="inner">
                 <h1 className="logo"><NavLink exact to="/">Amerimnos</NavLink><span>.</span></h1>
                 <ul className="gnb">
-                    <li><NavLink exact activeStyle={clickStyle} to="/department">Department</NavLink></li>
-                    <li><NavLink exact activeStyle={clickStyle} to="/community">Community</NavLink></li>
-                    <li><NavLink exact activeStyle={clickStyle} to="/gallery">Gallery</NavLink></li>
-                    <li><NavLink exact activeStyle={clickStyle} to="/youtube">Youtube</NavLink></li>
-                    <li><NavLink exact activeStyle={clickStyle} to="/location">Location</NavLink></li>
+                    <li ref={line} className="line"></li>
+                    <li><NavLink exact activeStyle={clickStyle} onMouseEnter={lineMove} to="/department">Department</NavLink></li>
+                    <li><NavLink exact activeStyle={clickStyle} onMouseEnter={lineMove} to="/community">Community</NavLink></li>
+                    <li><NavLink exact activeStyle={clickStyle} onMouseEnter={lineMove} to="/gallery">Gallery</NavLink></li>
+                    <li><NavLink exact activeStyle={clickStyle} onMouseEnter={lineMove} to="/youtube">Youtube</NavLink></li>
+                    <li><NavLink exact activeStyle={clickStyle} onMouseEnter={lineMove} to="/location">Location</NavLink></li>
                 </ul>
                 <NavLink className="join" exact activeStyle={clickStyle} to="/Join">
                     Join <span class="material-icons-round">arrow_forward_ios</span>
