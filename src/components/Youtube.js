f192a19aed6aa97f355485cb2ef0bfdc9f19286f
yanggitak <yanggitak@naver.com>
2022-01-10 23:03:32 +0900

    유투브  pc형 형태 완료

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -8,30 +8,30 @@ function Youtube() {
                     <ul className="menu">
                         <li className="tit">MENU</li>
                         <li className="item">
-                            <span>아이콘</span>
+                            <span class="material-icons-outlined">home</span>
                             Discover
                         </li>
                         <li className="item">
-                            <span>아이콘</span>
+                            <span class="material-icons-outlined">trending_up</span>
                             Trending
                         </li>
                         <li className="item">
-                            <span>아이콘</span>
+                            <span class="material-icons-outlined">bookmark</span>
                             Playlist
                         </li>
                     </ul>
                     <ul className="category">
                         <li className="tit">CATEGORY</li>
                         <li className="item">
-                            <span>아이콘</span>
+                            <span class="material-icons-outlined">live_tv</span>
                             Live Stream
                         </li>
                         <li className="item">
-                            <span>아이콘</span>
+                            <span class="material-icons-outlined">park</span>
                             Nature
                         </li>
                         <li className="item">
-                            <span>아이콘</span>
+                            <span class="material-icons-outlined">library_music</span>
                             Music
                         </li>
                     </ul>
@@ -41,54 +41,77 @@ function Youtube() {
                         <label htmlFor="youtubeSearch">
                             Youtube Search button
                             <input type="text" name="youtubeSearch" id="youtubeSearch" placeholder="Search" />
-                            <input type="button" value="" />
+                            <input type="button" value="youtube search button" />
                         </label>
                         <div className="theme">
-                            <button type="button"></button>
+                            <input type="checkbox" name="" id="themeCheck" />
+                            <label htmlFor="themeCheck"></label>
                             <span>Night Mode</span>
                         </div>
                     </div>
-                    <h2>Discover</h2>
+                    <h2 className="constTit">Discover</h2>
                     <ul className="videosConts1">
-                        <li>
-                            <div className="videoWrap">
-                                <iframe src="test" frameborder="0" title="videos"></iframe>
-                            </div>
+                        <li className="item1">
+                            <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             <div className="tit">How to do Basic Jumping and how to lading safely</div>
+                            <div className="conts">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, consequuntur!</div>
                         </li>
-                        <li>
-                            <div className="videoWrap">
-                                <iframe src="test" frameborder="0" title="videos"></iframe>
-                            </div>
+                        <li className="item2">
+                            <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             <div className="tit">How to do Basic Jumping and how to lading safely</div>
                         </li>
                     </ul>
-                    <h2>Discover</h2>
+                    <h2 className="constTit2">Most Watched</h2>
                     <ul className="videosConts2">
-                        <li>
-                            <div className="videoWrap">
-                                <iframe src="test" frameborder="0" title="videos"></iframe>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                             <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
                         </li>
-                        <li>
-                            <div className="videoWrap">
-                                <iframe src="test" frameborder="0" title="videos"></iframe>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                             <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
                         </li>
-                        <li>
-                            <div className="videoWrap">
-                                <iframe src="test" frameborder="0" title="videos"></iframe>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                             <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
                         </li>
-                        <li>
-                            <div className="videoWrap">
-                                <iframe src="test" frameborder="0" title="videos"></iframe>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                             <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
                         </li>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
+                        </li>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
+                        </li>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
+                        </li>
+                        <li className="item">
+                            <div className="iframeWrap">
+                                <iframe src="https://www.youtube.com/embed/KMHgowSN-Cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div className="bottomTxt">How to do Basic Jumping and how to lading safely</div>
+                        </li>
+
                     </ul>
                 </main>
             </div>
