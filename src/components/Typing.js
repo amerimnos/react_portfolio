a74694ff78301a3696a184ce3771532a49d30f4c
yanggitak <yanggitak@naver.com>
2022-02-13 15:03:38 +0900

    Typing 효과를 직접 class처리 하여 플로그인 처럼 사용할 수 있게 스크립트 처리 완료.

---
+++ src/components/Typing.js
@@ -0,0 +1,52 @@
+
+
+class Typing {
+    
+    constructor(ref, array) {
+
+        this.ref = ref.current;
+        this.newObj = [];
+        array.forEach((element, index) => {
+            this.newObj = [...this.newObj, element.split('')];
+        });
+        this.typing();
+    }
+
+    async typing() {
+        for (let arrayIndex = 0; arrayIndex < this.newObj.length; arrayIndex++) {
+
+            await new Promise(
+                resolve => {
+                    for (let index = 0; index < this.newObj[arrayIndex].length; index++) {
+                        setTimeout(() => {
+                            let text = this.newObj[arrayIndex][index];
+                            this.ref.textContent += text;
+                        }, 50 * index);
+                    }
+                    setTimeout(() => {
+                        resolve();
+                    }, 50 * this.newObj[arrayIndex].length + 2000);
+                }
+            )
+            if (arrayIndex !== this.newObj.length - 1) {
+                await new Promise(
+                    resolve => {
+                        for (let index = this.newObj[arrayIndex].length, i = 0; 0 < index; index--) {
+                            setTimeout(() => {
+                                this.newObj[arrayIndex].splice(index - 1, 1 + i);
+                                this.ref.textContent = this.newObj[arrayIndex].join('');
+                            }, 100 * i);
+                            i++
+                        }
+                        setTimeout(() => {
+                            resolve();
+                        }, 100 * this.newObj[arrayIndex].length + 1000);
+                    }
+                )
+            }
+
+        }
+    }
+}
+
+export default Typing;
--- src/components/main/MainConts1.js
+++ src/components/main/MainConts1.js
@@ -1,31 +1,28 @@
-/* import { useEffect, useRef } from 'react'; */
+
+import { useEffect, useRef } from 'react';
 import { ReactComponent as BgIcon1 } from '../main_bg.svg';
-export function MainConts1(/* props */) {
+import Typing from '../Typing.js';
+function MainConts1() {
     const url = process.env.PUBLIC_URL;
+    let typedTxt = useRef(null);
 
-    /* let conts1 = useRef(null);
-    useEffect(
-        () => {
-            props.SetPos1(conts1.current.offsetTop)
-            window.addEventListener('resize', () => {
-                props.SetPos1(conts1.current.offsetTop)
-            })
 
-            return (
-                ()=>{
-                    window.removeEventListener('resize', () => {
-                        props.SetPos1(conts1.current.offsetTop)
-                    })
-                }
-            )
-        }, []
-    ) */
+
+    useEffect(() => {
+        new Typing(typedTxt, [
+            "We build digital products for creative peoples",
+            "Love yourself",
+            "Golden rule",
+        ]);
+
+    }, [])
+
 
     return (
         <section /* ref={conts1} */ className="mainConts1">
             <div className="inner">
                 <div className="topText">We are a digital agency based on Korea</div>
-                <h2>We build digital products for creative peoples</h2>
+                <h2 ref={typedTxt}></h2>
                 <div className="bottomTxt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eos fugit ipsa.</div>
                 <a href="#test" className="btn">SEE OUR WORKS
                     <span className="arrow"></span>
